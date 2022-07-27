6861d77b373b4475f05b909f70d213b259b7bc61
ollylll97lll <58421531+ollylll97lll@users.noreply.github.com>
2022-07-27 13:53:14 +0700

    Update MQConsumer.java

--- SpringB Project/springbootmserv/microservices/itemserv/src/main/java/com/r00t/itemserv/consumer/MQConsumer.java
+++ SpringB Project/springbootmserv/microservices/itemserv/src/main/java/com/r00t/itemserv/consumer/MQConsumer.java
@@ -1,9 +1,7 @@
 package com.r00t.itemserv.consumer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import com.r00t.itemserv.models.DTO.responsebody.QuantityUpdateDTO;
